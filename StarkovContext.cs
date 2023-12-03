a06b318bba839b0552762cd83e70b254ae3e2811
chrisrekfild <chrisrekfild@mail.ru>
2023-12-03 23:35:58 +0500

    Обновление с Бэкапом БД

--- StarkovContext.cs
+++ StarkovContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Xml;
 
 namespace Starkov_v1;
 
@@ -22,8 +23,32 @@ public partial class StarkovContext : DbContext
     public virtual DbSet<JobTitle> JobTitles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=STARKOV;Username=postgres;Password=2113");
+    {
+        string connString = string.Empty;
+
+        XmlDocument xDoc = new XmlDocument();
+        string path = Directory.GetCurrentDirectory() + "\\Config\\Configuration.xml";
+        xDoc.Load(path);
+
+        XmlElement? xRoot = xDoc.DocumentElement;
+        if (xRoot != null)
+        {
+            // обход всех узлов в корневом элементе
+            foreach (XmlElement xnode in xRoot)
+            {
+                // получаем атрибут name
+                XmlNode? attr = xnode.Attributes.GetNamedItem("ConnectionString");
+                connString = attr?.Value;
+
+                int a = 0;
+                a++;
+            }
+        }
+
+                optionsBuilder.UseNpgsql(connString);
+    }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
